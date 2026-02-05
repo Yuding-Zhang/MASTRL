22f0be8c740930fe123fbf2cbe6446414aa52c91
yuding <steven_yuding@163.com>
2026-02-05 06:55:29 +0000

    关键字参数，避免位置错乱

--- mastrl/utils/shared_buffer.py
+++ mastrl/utils/shared_buffer.py
@@ -97,8 +97,8 @@ class SharedReplayBuffer(object):
         self.step = 0
 
     def insert(self, share_obs, obs, rnn_states_actor, rnn_states_critic, actions, action_log_probs,
-               value_preds, rewards, masks, bad_masks=None, active_masks=None, available_actions=None,
-               z=None, credit_logits=None):
+               value_preds, rewards, masks, z=None, credit_logits=None, bad_masks=None, 
+               active_masks=None, available_actions=None):
         """
         Insert data into the buffer.
         :param share_obs: (argparse.Namespace) arguments containing relevant model, policy, and env information.
